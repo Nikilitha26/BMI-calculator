3c40468d096a1a36d79bc32b00dec65ee4f9a5cd
Nikilitha <kakazanikilitha544@gmail.com>
2025-02-17 10:47:49 +0200

    responsiveness

--- bmi.js
+++ bmi.js
@@ -3,8 +3,12 @@ let heightInput = document.getElementById('height');
 let weightInput = document.getElementById('weight');
 let resultElement = document.getElementById('bmi');
 
-calculateButton.addEventListener('click', () =>{
-    resultElement.value = eval(`${weightInput.value} / (${heightInput.value * heightInput.value})`);
-    
-})
+calculateButton.addEventListener('click', () => {
+    let height = heightInput.value / 100; // Convert height from cm to meters
+    let weight = weightInput.value;
+    let bmi = weight / (height * height);
+
+    resultElement.value = bmi.toFixed(2); // Round to 2 decimal places
+});
+
 
